1f7a1c367192912b6f8b8887cd9d418eb2db26b4
CHEMICALS1234 <ysmsmart1@gmail.com>
2021-03-04 11:40:54 +0900

    Avoid Hoisting

--- physics/oscillation_physics.js
+++ physics/oscillation_physics.js
@@ -1,147 +1,161 @@
 // import * as dat from './js/dat.gui.min.js'
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
 
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(0x000000);
-renderer.setSize( window.innerWidth , window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 //make window be able to be resized sans gene
-window.addEventListener( 'resize', function(){
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
+window.addEventListener('resize', () => {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
 });
 
-var controls = new THREE.OrbitControls( camera, renderer.domElement );
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-var light = new THREE.DirectionalLight( 0xffffff );
-light.position.set( 0,1,1 ).normalize();
+const light = new THREE.DirectionalLight(0xffffff);
+light.position.set(0, 1, 1).normalize();
 scene.add(light);
 
-var geometry = new THREE.SphereGeometry(5,100,100);
-var material = new THREE.MeshPhongMaterial({
-    color : 0xFE98A0,
-    flatShading : true
+const geometry = new THREE.SphereGeometry(5, 100, 100);
+const material = new THREE.MeshPhongMaterial({
+  color: 0xfe98a0,
+  flatShading: true,
 });
-var sphere = new THREE.Mesh(geometry, material);
+const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
 //axisHelper
-var axesHelper = new THREE.AxesHelper( 100 );
-scene.add( axesHelper );
-
+const axesHelper = new THREE.AxesHelper(100);
+scene.add(axesHelper);
 
 camera.position.z = 100;
 camera.position.x = 100;
 camera.position.y = 100;
 camera.lookAt(scene.position);
 
-
 //dat.gui objects
-var guiControls_X = new function(){
-    this.b = 0;
-    this.k = 10;
-    this.F0 = 10;
-    this.w = 1;
+const guiControls_X = {
+  b: 0,
+  k: 10,
+  F0: 10,
+  w: 1,
 };
 
-var guiControls_Y = new function(){
-    this.b = 0;
-    this.k = 20;
-    this.F0 = 10;
-    this.w = 1;
+const guiControls_Y = {
+  b: 0,
+  k: 20,
+  F0: 10,
+  w: 1,
 };
 
-var guiControls_Z = new function(){
-    this.b = 0;
-    this.k = 30;
-    this.F0 = 10;
-    this.w = 1;
+const guiControls_Z = {
+  b: 0,
+  k: 30,
+  F0: 10,
+  w: 1,
 };
 
-var guiMass = new function(){
-    this.m = 1;
-}
-
-var colour =  {
-    color: 0xFE98A0
+const guiMass = {
+  m: 1,
 };
 
-var gui = new dat.GUI();
-let gui_X = gui.addFolder('X_motion');
-let gui_Y = gui.addFolder('Y_motion');
-let gui_Z = gui.addFolder('Z_motion');
+const colour = {
+  color: 0xfe98a0,
+};
 
-gui.add(guiMass, 'm' , 0.1, 10);
+const gui = new dat.GUI();
+const gui_X = gui.addFolder('X_motion');
+const gui_Y = gui.addFolder('Y_motion');
+const gui_Z = gui.addFolder('Z_motion');
 
-gui_X.add(guiControls_X, 'b',0,10);
-gui_X.add(guiControls_X, 'k',0,100);
-gui_X.add(guiControls_X, 'F0',0,100);
-gui_X.add(guiControls_X, 'w',0,100);
+gui.add(guiMass, 'm', 0.1, 10);
 
-gui_Y.add(guiControls_Y, 'b',0,10);
-gui_Y.add(guiControls_Y, 'k',0,100);
-gui_Y.add(guiControls_Y, 'F0',0,100);
-gui_Y.add(guiControls_Y, 'w',0,100);
+gui_X.add(guiControls_X, 'b', 0, 10);
+gui_X.add(guiControls_X, 'k', 0, 100);
+gui_X.add(guiControls_X, 'F0', 0, 100);
+gui_X.add(guiControls_X, 'w', 0, 100);
 
-gui_Z.add(guiControls_Z, 'b',0,10);
-gui_Z.add(guiControls_Z, 'k',0,100);
-gui_Z.add(guiControls_Z, 'F0',0,100);
-gui_Z.add(guiControls_Z, 'w',0,100);
+gui_Y.add(guiControls_Y, 'b', 0, 10);
+gui_Y.add(guiControls_Y, 'k', 0, 100);
+gui_Y.add(guiControls_Y, 'F0', 0, 100);
+gui_Y.add(guiControls_Y, 'w', 0, 100);
 
-gui.addColor( colour, 'color' ).onChange( function() { sphere.material.color.set( colour.color ); } );
+gui_Z.add(guiControls_Z, 'b', 0, 10);
+gui_Z.add(guiControls_Z, 'k', 0, 100);
+gui_Z.add(guiControls_Z, 'F0', 0, 100);
+gui_Z.add(guiControls_Z, 'w', 0, 100);
 
+gui.addColor(colour, 'color').onChange(() => {
+  sphere.material.color.set(colour.color);
+});
 
-function Data(location, accel, vel){
+class Data {
+  constructor(location, accel, vel) {
     this.location = location;
     this.accel = accel;
     this.vel = vel;
-};
+  }
+}
 
 const dt = 0.025;
 
-var t = 0;
+let t = 0;
 
 //define X,Y,Z
-let X = new Data(20,0,0);
-let Y = new Data(20,0,0);
-let Z = new Data(50,0,0);
+let X = new Data(20, 0, 0);
+let Y = new Data(20, 0, 0);
+let Z = new Data(50, 0, 0);
 
-sphere.position.set(X.location,Y.location,Z.location);
+sphere.position.set(X.location, Y.location, Z.location);
 
 //draw scene
-var render = function(){
-    renderer.render(scene,camera);
+const render = () => {
+  renderer.render(scene, camera);
+};
+
+const objectMotion_euler = (obj, guiControls, index) => {
+  obj.vel += obj.accel * dt;
+  switch (index) {
+    case 0:
+      sphere.position.x += obj.vel * dt;
+      break;
+    case 1:
+      sphere.position.y += obj.vel * dt;
+      break;
+    case 2:
+      sphere.position.z += obj.vel * dt;
+  }
+  obj.location = sphere.position.getComponent(index);
+  obj.accel =
+    -(
+      guiControls.b * obj.vel +
+      guiControls.k * obj.location +
+      guiControls.F0 * Math.cos(guiControls.w * t)
+    ) / guiMass.m;
 };
 
 //game loop
-var GameLoop = function(){
+const GameLoop = () => {
+  requestAnimationFrame(GameLoop);
 
-    requestAnimationFrame(GameLoop);
+  objectMotion_euler(X, guiControls_X, 0);
+  objectMotion_euler(Y, guiControls_Y, 1);
+  objectMotion_euler(Z, guiControls_Z, 2);
 
-    X.vel += X.accel*dt;
-    sphere.position.x += X.vel * dt;
-    X.location = sphere.position.getComponent(0);
-    X.accel = -(guiControls_X.b*X.vel + guiControls_X.k*X.location + guiControls_X.F0*Math.cos(guiControls_X.w*t))/guiMass.m;
-
-    Y.vel += Y.accel*dt;
-    sphere.position.y += Y.vel * dt;
-    Y.location = sphere.position.getComponent(1);
-    Y.accel = -(guiControls_Y.b*Y.vel + guiControls_Y.k*Y.location + guiControls_Y.F0*Math.cos(guiControls_Y.w*t))/guiMass.m;
-
-    Z.vel += Z.accel*dt;
-    sphere.position.z += Z.vel * dt;
-    Z.location = sphere.position.getComponent(2);
-    Z.accel = -(guiControls_Z.b*Z.vel + guiControls_Z.k*Z.location + guiControls_Z.F0*Math.cos(guiControls_Z.w*t))/guiMass.m;
-
-    t += dt;
-    console.log(t)
-    render();
-}
+  t += dt;
+  render();
+};
 
 GameLoop();
